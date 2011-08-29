71a22e54309709239c6783cd7f8e2b318dab58e6
Fabian Holler <git@fholler.de>
2011-08-29 18:59:22 +0200

    doxygen_examples/*: removed license header

--- doxygen_examples/ssh_tr.c
+++ doxygen_examples/ssh_tr.c
@@ -1,25 +1,3 @@
-/*
- * This file is part of RTRlib.
- *
- * RTRlib is free software; you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation; either version 3 of the License, or (at your
- * option) any later version.
- *
- * RTRlib is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
- * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
- * License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with RTRlib; see the file COPYING.LESSER.
- *
- * written by Fabian Holler, in cooperation with:
- * INET group, Hamburg University of Applied Sciences,
- * CST group, Freie Universitaet Berlin
- * Website: http://rpki.realmv6.org/
- */
-
 #include <stdlib.h>
 #include "rtrlib/transport/ssh/ssh_transport.h"
 
