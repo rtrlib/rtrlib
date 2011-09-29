5931c9377624c0d01be4aa4a267d97806c55351a
Fabian Holler <git@fholler.de>
2011-09-29 22:58:01 +0200

    tools/mgr_test.c: added rtr ssh socket to group

--- tools/mgr_test.c
+++ tools/mgr_test.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <stdio.h>
 #include "rtrlib/rtrlib.h"
 #include <rtrlib/rtr_mgr.h>
 
@@ -7,17 +8,28 @@ int main(){
     pfx_table_init(&pfxt, NULL, 0);
 
     tr_socket* tr_tcp;
-    tr_socket* tr_tcp1;
+    tr_socket* tr_ssh;
     tr_tcp_config tcp_config = {
         "141.22.26.232",          //IP
         "42420"                      //Port
     };
+
     tr_tcp_init(&tcp_config, &tr_tcp);
     rtr_socket rtr_tcp;
     rtr_init(&rtr_tcp, tr_tcp, &pfxt, 240, 480);
-    tr_tcp_init(&tcp_config, &tr_tcp1);
-    rtr_socket rtr_tcp1;
-    rtr_init(&rtr_tcp1, tr_tcp, &pfxt, 240, 480);
+
+    tr_ssh_config ssh_config = {
+        "141.22.26.232",
+        22,
+        "fho",
+        NULL,
+        "/tmp/key",
+        "/tmp/key.pub"
+    };
+
+    tr_ssh_init(&ssh_config, &tr_ssh);
+    rtr_socket rtr_ssh;
+    rtr_init(&rtr_ssh, tr_ssh, &pfxt, 10, 480);
 
 
     rtr_mgr_config conf[2];
@@ -25,17 +37,17 @@ int main(){
         conf[i].sockets = malloc(1 * sizeof(rtr_socket*));
         if(i==0){
             conf[i].preference = 2;
-            conf[i].sockets[0] = &rtr_tcp;
+            conf[i].sockets[0] = &rtr_ssh;
         }
         else if(i ==1){
             conf[i].preference = 3;
-            conf[i].sockets[0] = &rtr_tcp1;
+            conf[i].sockets[0] = &rtr_tcp;
         }
         conf[i].sockets_len = 1;
     }
 
     rtr_mgr_init(conf, 2);
-    rtr_mgr_start(conf, 2);
+    rtr_mgr_start(conf);
     printf("started\n");
     sleep(500000);
 }
