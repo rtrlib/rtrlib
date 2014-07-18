579b0ced6104a5b5cb0ba8f6d7220b568c1db3b6
Nils Bars <nils_bars@t-online.de>
2014-07-18 13:37:42 +0200

    Change testfiles compiler flags and add tests
    -The testfiles now use the same compiler flags like die lib.
    -Add a spki_record compare function to the ht-spkitable test file.
    -Add tests

--- tests/test_ht_spkitable.c
+++ tests/test_ht_spkitable.c
@@ -17,11 +17,33 @@
 #include "rtrlib/spki/hashtable/ht-spkitable.h"
 #include <stdio.h>
 #include <string.h>
+#include <time.h>
+#include <stdlib.h>
 
 
-struct spki_record *create_record(int ASN, int ski_offset, int spki_offset, struct rtr_socket *socket);
+static struct spki_record *create_record(int ASN, int ski_offset, int spki_offset, struct rtr_socket *socket);
 
-struct spki_record *create_record(int ASN, int ski_offset, int spki_offset, struct rtr_socket *socket) {
+
+
+
+
+static bool compare_spki_records(struct spki_record *r1, struct spki_record *r2){
+    if(r1->asn != r2->asn){
+        return false;
+    }
+    if(r1->socket != r2->socket){
+        return false;
+    }
+    if(memcmp(r1->ski, r2->ski, SKI_SIZE) != 0){
+        return false;
+    }
+    if(memcmp(r1->spki, r2->spki, SPKI_SIZE) != 0){
+        return false;
+    }
+    return true;
+}
+
+static struct spki_record *create_record(int ASN, int ski_offset, int spki_offset, struct rtr_socket *socket) {
     struct spki_record *record = malloc(sizeof(struct spki_record));
     record->asn = ASN;
     uint32_t i;
@@ -37,7 +59,7 @@ struct spki_record *create_record(int ASN, int ski_offset, int spki_offset, stru
     return record;
 }
 
-static void test_add(){
+static void test_add_1(){
     struct spki_table table;
     spki_table_init(&table,NULL);
 
@@ -45,7 +67,7 @@ static void test_add(){
     struct rtr_socket *socket_two = malloc(sizeof(struct rtr_socket));
     //Create 255 records with same ASN and SKI, different SPKI. Add them
     struct spki_record *record;
-    for(int i = 0; i< 255;i++){
+    for(unsigned int i = 0; i< 255;i++){
         if(i%2 != 0)
             record = create_record(1,0,i,socket_one);
         if(i%2 == 0)
@@ -55,39 +77,89 @@ static void test_add(){
 
     }
 
-
-
     struct spki_record *result;
-    spki_table_get_all(&table, record->asn, record->ski,&result);
+    unsigned int result_size;
+    spki_table_get_all(&table, record->asn, record->ski,&result, &result_size);
 
     int count = 0;
-    while(result){
+
+    for(unsigned int i = 0; i < result_size; i++){
         assert(result->asn == record->asn);
-        assert(memcmp(result->ski,record->ski,SKI_SIZE) == 0);
-        result = result->next;
+        assert(memcmp(&result[i].ski,record->ski,SKI_SIZE) == 0);
+        count++;
+    }
+    assert(count == 255);
+
+    spki_table_src_remove(&table,socket_one);
+    spki_table_get_all(&table, record->asn, record->ski,&result,&result_size);
+
+    for(unsigned int i = 0; i < result_size; i++){
+        assert(result->asn == record->asn);
+        assert(memcmp(&result[i].ski,record->ski,SKI_SIZE) == 0);
         count++;
     }
 
-    assert(count == 255);
+    spki_table_free(&table);
+    free(result);
+
+    printf("test_add_1() complete\n");
+}
+
+static void test_add_2(){
+    struct spki_table table;
+    spki_table_init(&table,NULL);
+
+    //Test: Two diff SPKIs hash to the same SKI
+    struct spki_record *record1 = create_record(10,20,30,NULL);
+    struct spki_record *record2 = create_record(10,20,40,NULL);
 
 
-    spki_table_src_remove(&table,socket_one);
-    spki_table_get_all(&table, record->asn, record->ski,&result);
+    assert(spki_table_add_entry(&table, record1) == SPKI_SUCCESS);
+    assert(spki_table_add_entry(&table, record2) == SPKI_SUCCESS);
 
-    while(result){
-        assert(result->socket == socket_two);
-        result = result->next;
-    }
+    struct spki_record *result;
+    unsigned int result_size;
+    spki_table_get_all(&table, 10, record1->ski, &result, &result_size);
+    assert(result_size == 2);
 
+    assert(compare_spki_records(&result[0], record1) != compare_spki_records(&result[0], record2));
+    assert(compare_spki_records(&result[1], record1) != compare_spki_records(&result[1], record2));
+    free(result);
+
+    spki_table_remove_entry(&table, record1);
+    spki_table_get_all(&table, 10, record1->ski, &result, &result_size);
+    assert(result_size == 1);
+
+    assert(compare_spki_records(&result[0], record2));
+    free(result);
+
+    spki_table_remove_entry(&table, record2);
+    spki_table_get_all(&table, 10, record1->ski, &result, &result_size);
+    assert(result_size == 0);
+    assert(result == NULL);
 
     spki_table_free(&table);
 
-    free(result);
+    //Test: Add equal records
+    spki_table_init(&table,NULL);
+    record1 = create_record(10,10,10,NULL);
+    record2 = create_record(10,10,10,NULL);
 
+    assert(spki_table_add_entry(&table, record1) == SPKI_SUCCESS);
+    assert(spki_table_add_entry(&table, record2) == SPKI_DUPLICATE_RECORD);
+    assert(spki_table_add_entry(&table, record1) == SPKI_DUPLICATE_RECORD);
 
+    spki_table_get_all(&table, 10, record1->ski, &result, &result_size);
+    assert(result_size == 1);
+
+    spki_table_free(&table);
+    printf("test_add_2() complete\n");
 }
 
+
 int main(){
-    test_add();
+    test_add_1();
+    test_add_2();
+
     return EXIT_SUCCESS;
 }
