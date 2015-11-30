029d39e8fc5ea5f553ccda735c8e9594dd7d20e9
Nils Bars <nils_bars@t-online.de>
2015-11-30 16:45:41 +0100

    Add debug output

--- rtrlib/rtr/rtr.c
+++ rtrlib/rtr/rtr.c
@@ -206,6 +206,7 @@ void rtr_fsm_start(struct rtr_socket *rtr_socket)
             RTR_DBG1("State: RTR_ERROR_TRANSPORT");
             tr_close(rtr_socket->tr_socket);
             rtr_change_socket_state(rtr_socket, RTR_CONNECTING);
+            RTR_DBG("Waiting %u", rtr_socket->retry_interval);
             sleep(rtr_socket->retry_interval);
         }
 
@@ -213,6 +214,7 @@ void rtr_fsm_start(struct rtr_socket *rtr_socket)
             RTR_DBG1("State: RTR_ERROR_FATAL");
             tr_close(rtr_socket->tr_socket);
             rtr_change_socket_state(rtr_socket, RTR_CONNECTING);
+            RTR_DBG("Waiting %u", rtr_socket->retry_interval);
             sleep(rtr_socket->retry_interval);
         }
 
