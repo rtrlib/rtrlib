fbe7c72fa76bc032d9f303aa8158bcb4da87bc87
smlng <s@mlng.net>
2016-04-08 14:51:47 +0200

    travis with libssh and tests
    - update travis.yml
    - add live validation test
    - run make test on travis
    - build with and w/o libssh
    - disabled test_pfx_locks

--- scripts/cppcheck.sh
+++ scripts/cppcheck.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-cppcheck --error-exitcode=1--enable=all -i rtrlib/spki/hashtable/tommyds-1.8/ tools/ rtrlib/
+cppcheck --error-exitcode=1 --enable=all --inline-suppr \
+	--template='{id}:{file}({line}):({severity}) {message}' \
+	-i rtrlib/spki/hashtable/tommyds-1.8/ tools/ rtrlib/
