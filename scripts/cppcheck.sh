7a27b6832f2c0742fa81a14dfd2d61edf75f6ebb
Fabian Holler <mail@fholler.de>
2016-04-11 20:22:50 +0700

    scripts: supress unwated cppcheck warning in ip.c
    Cppcheck warned:
    	[rtrlib/lib/ip.c:66]: (style) The function 'lrtr_ip_str_cmp'
    	is never used.
    
    The function is used by external programs, like test programs that are
    not checked by cppcheck. Keep the function, supress the warning

--- scripts/cppcheck.sh
+++ scripts/cppcheck.sh
@@ -3,4 +3,5 @@
 cppcheck --error-exitcode=1 --enable=all \
 	--suppress="duplicateExpression:rtrlib/rtr/packets.c:974" \
 	--suppress="duplicateExpression:rtrlib/rtr/packets.c:976" \
+	--suppress="unusedFunction:rtrlib/lib/ip.c:66" \
 	-i rtrlib/spki/hashtable/tommyds-1.8/ tools/ rtrlib/
