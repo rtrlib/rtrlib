49315c82cce467b997a2d742337c693cf87eb7d3
Fabian Holler <mail@fholler.de>
2016-04-11 20:29:17 +0700

    scripts: include cppcheck problem id in output
    Include the id of the reported problem in the cppcheck output.
    This makes it easier to find out the ID for supressing messages.

--- scripts/cppcheck.sh
+++ scripts/cppcheck.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 cppcheck --error-exitcode=1 --enable=all \
+	--template='{id}:{file}({line}):({severity}) {message}' \
 	--suppress="duplicateExpression:rtrlib/rtr/packets.c:974" \
 	--suppress="duplicateExpression:rtrlib/rtr/packets.c:976" \
 	--suppress="unusedFunction:rtrlib/lib/ip.c:66" \
