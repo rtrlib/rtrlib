c2c8eb89021b35a0201ec6e1e59903780b0ed75a
Fabian Holler <mail@fholler.de>
2016-04-11 19:02:13 +0700

    scripts: supress cppcheck duplicateExpression warning in packets.c
    cppcheck was warning:
    	[rtrlib/rtr/packets.c:974] -> [rtrlib/rtr/packets.c:974]: (style) Same
    	expression on both sides of '||'.
    	[rtrlib/rtr/packets.c:976] -> [rtrlib/rtr/packets.c:976]: (style) Same
    	expression on both sides of '||'.
    The expression checks if the rtval is the same than enum error values,
    that are represented internally as the same integer values.
    It should explicitly check for the enum values to ensure that the code
    still works if the order of the enum changes.
    
    Therefore suppress the cppcheck warning.

--- scripts/cppcheck.sh
+++ scripts/cppcheck.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-cppcheck --error-exitcode=1 --enable=all -i rtrlib/spki/hashtable/tommyds-1.8/ tools/ rtrlib/
+cppcheck --error-exitcode=1 --enable=all \
+	--suppress="duplicateExpression:rtrlib/rtr/packets.c:974" \
+	--suppress="duplicateExpression:rtrlib/rtr/packets.c:976" \
+	-i rtrlib/spki/hashtable/tommyds-1.8/ tools/ rtrlib/
