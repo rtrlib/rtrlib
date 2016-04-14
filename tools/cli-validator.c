52b4d2f2703b264634c63d28325aac771e9951a9
smlng <s@mlng.net>
2016-04-14 14:47:44 +0200

    add tools to coding style check

--- tools/cli-validator.c
+++ tools/cli-validator.c
@@ -1,3 +1,12 @@
+/*
+ * This file is part of RTRlib.
+ *
+ * This file is subject to the terms and conditions of the MIT license.
+ * See the file LICENSE in the top level directory for more details.
+ *
+ * Website: http://rtrlib.realmv6.org/
+*/
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
