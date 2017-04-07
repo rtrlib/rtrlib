7e6c78beaed7636206b6c3f301f135285703b973
Marcel Röthke <marcel.roethke@haw-hamburg.de>
2017-04-07 14:23:04 +0200

    ci: ensure normal build steps are not run if coverity runs
    This is necessary because the regular ci steps may fail in unpredictable
    ways if coverity is active.

---
+++ scripts/travis.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env sh
+
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
+
+function run_command {
+	eval $@
+
+	ret=$?
+	if [ $ret != 0 ]; then
+		colour=$RED
+	else
+		colour=$GREEN
+	fi
+	echo -e "\n${colour}The command \"$@\" exited with $ret.$NC\n\n"
+}
+
+run_command scripts/cppcheck.sh
+run_command scripts/check-coding-style.sh
+run_command cmake -D CMAKE_BUILD_TYPE=NoSSH .
+run_command make
+run_command make test
+run_command make clean
+run_command cmake -D CMAKE_BUILD_TYPE=Release -DENABLE_COVERAGE=On -DUNIT_TESTING=On .
+run_command make
+run_command make test
+run_command make gcov
