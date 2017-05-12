rtrlib/rtrlib

commit 7c6ba5ccf8791a954d31a094bddc9858c381df84

rtr_mgr: Fix memory leak in rtr_mgr_init
Due to missing validation of the groups_len parameter in rtr_mgr_init
memory allocated for the pfx and spki tables could leak. In the unlikely
case that groups_len is zero no pointer to it would exist after the function
returns. Usually they are passed to rtr_init and stored in the
rtr_sockets, but that does not happen when groups_len is null, since
there is no socket to initialize in that case.

Furthermore this commit fixes a bug in the cleanup which caused a freed
pointer to be returned through config_out, in case of an error
situation.

coverity ids: 1420791 1420783

rtrlib/rtr_mgr.c
@@ -304,6 +304,11 @@ int rtr_mgr_init(struct rtr_mgr_config **config_out,
 		goto err;
 	}
 
+	if (groups_len == 0) {
+		MGR_DBG1("Error Empty rtr_mgr_group array");
+		goto err;
+	}
+
 	config->len = groups_len;
 	config->groups = malloc(groups_len * sizeof(*groups));
 	if (!config->groups)
@@ -370,6 +375,7 @@ err:
 	free(config->groups);
 	free(config);
 	config = NULL;
+	*config_out = NULL;
 	return err_code;
 }
 

rtrlib/rtr_mgr.h
@@ -90,7 +90,7 @@ struct rtr_mgr_config {
  *		     associated with one rtr socket. The preference values must
  *		     be unique in the group array. More than one rtr_mgr_group
  *		     with the same preference value isn't allowed.
- * @param[in] groups_len Number of elements in the groups array.
+ * @param[in] groups_len Number of elements in the groups array. Must be >= 1.
  * @param[in] refresh_interval Interval in seconds between serial queries that
  *			     are sent to the server. Must be >= 1 and <=
  *			     86400s (1d), recommended default is 3600s (1h).

commit bc74a8f84bd2532d3a91345d00ef488fc9535982

rtr_mgr: fix uninitialized pointer read in rtr_mgr_init
This fixes a bug introduced by 7c6ba5ccf8791a954d31a094bddc9858c381df84. Due to the way cleanup on
error is handled an uninitialized pointer is passed to free if the
execution jumps to err before config->groups is initialized. This
can happen when group_len is zero or when pthread_mutex_init fails.

This commit fixes this by reordering the instructions and returning
early if group_len is zero, because config->groups cannot be initialized
before group_len is validated. The other approach, using different error
labels, would unnecessarily complicate error handling.

coverity id: 1434592

rtrlib/rtr_mgr.c
@@ -293,27 +293,27 @@ int rtr_mgr_init(struct rtr_mgr_config **config_out,
 	struct pfx_table *pfxt = NULL;
 	struct spki_table *spki_table = NULL;
 	struct rtr_mgr_config *config = NULL;
-
 	*config_out = NULL;
-	*config_out = config = malloc(sizeof(*config));
-	if (!config)
-		return RTR_ERROR;
-
-	if (pthread_mutex_init(&config->mutex, NULL) != 0) {
-		MGR_DBG1("Mutex initialization failed");
-		goto err;
-	}
 
 	if (groups_len == 0) {
 		MGR_DBG1("Error Empty rtr_mgr_group array");
-		goto err;
+		return RTR_ERROR;
 	}
+
+	*config_out = config = malloc(sizeof(*config));
+	if (!config)
+		return RTR_ERROR;
 
 	config->len = groups_len;
 	config->groups = malloc(groups_len * sizeof(*groups));
 	if (!config->groups)
 		goto err;
 	memcpy(config->groups, groups, groups_len * sizeof(*groups));
+
+	if (pthread_mutex_init(&config->mutex, NULL) != 0) {
+		MGR_DBG1("Mutex initialization failed");
+		goto err;
+	}
 
 	/* sort array in asc preference order */
 	qsort(config->groups, config->len,