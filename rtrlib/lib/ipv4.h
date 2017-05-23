78efca736e8323ca0982b43d7a33f6fef446b41c
smlng <s@mlng.net>
2017-05-23 23:17:29 +0200

    coding-style: adapt rtrlib/lib/ipv4.h

--- rtrlib/lib/ipv4.h
+++ rtrlib/lib/ipv4.h
@@ -20,57 +20,71 @@
  * @param addr The IPv4 address.
  */
 struct lrtr_ipv4_addr {
-    uint32_t addr;
+	uint32_t addr;
 };
 
 /**
- * @brief Extracts number bits from the passed ipv4_addr, starting at bit number from. The bit with the highest
- * significance is bit 0. All bits that aren't in the specified range will be 0.
+ * @brief Extracts number bits from the passed ipv4_addr,
+ *
+ * Starting at bit number from. The bit with the highest significance is bit 0.
+ * All bits that aren't in the specified range will be 0.
+ *
  * @param[in] val ipv4_addr
  * @param[in] from Position of the first bit that is extracted.
  * @param[in] number How many bits will be extracted.
- * @returns An ipv4_addr, where all bits that aren't in the specified range are set to 0.
-*/
-struct lrtr_ipv4_addr lrtr_ipv4_get_bits(const struct lrtr_ipv4_addr *val, const uint8_t from, const uint8_t number);
+ *
+ * @returns An ipv4_addr, with all bits not in the specified range set to 0.
+ */
+struct lrtr_ipv4_addr lrtr_ipv4_get_bits(const struct lrtr_ipv4_addr *val,
+					 const uint8_t from,
+					 const uint8_t number);
 
 /**
- * Converts the passed IPv4 address in string representation to an ipv4_addr struct.
+ * @brief Converts ab IPv4 address from string to ipv4_addr struct.
+ *
  * @param[in] str Pointer to a string buffer.
  * @param[out] ip ipv4_addr
+ *
  * @result 0 on success
  * @result -1 on error
-*/
+ */
 int lrtr_ipv4_str_to_addr(const char *str, struct lrtr_ipv4_addr *ip);
 
 /**
- * Converts the passed ipv4_addr to string representation.
+ * @brief Converts an ipv4_addr struct to its string representation.
+ *
  * @param[in] ip ipv4_addr
- * @param[out] str Pointer to a string buffer must be at least INET_ADDRSTRLEN bytes long.
+ * @param[out] str Pointer to a string buffer, of at least INET_ADDRSTRLEN.
  * @param[in] length of *str
+ *
  * @result 0 on success
  * @result -1 on error
-*/
-int lrtr_ipv4_addr_to_str(const struct lrtr_ipv4_addr *ip, char *str, const unsigned int len);
+ */
+int lrtr_ipv4_addr_to_str(const struct lrtr_ipv4_addr *ip,
+			  char *str, const unsigned int len);
 
 /**
- * Compares two ipv4_addr structs.
+ * @brief Compares two ipv4_addr structs.
+ *
  * @param[in] a ipv4_addr
  * @param[in] b ipv4_addr
+ *
  * @return true if a == b
  * @return false if a != b
-*/
-bool lrtr_ipv4_addr_equal(const struct lrtr_ipv4_addr *a, const struct lrtr_ipv4_addr *b);
+ */
+bool lrtr_ipv4_addr_equal(const struct lrtr_ipv4_addr *a,
+			  const struct lrtr_ipv4_addr *b);
 
 /**
  * @ingroup util_h[{
- * Converts the passed IPv4 address byte order with the given function.
+ * @brief Converts the passed IPv4 address byte order with the given function.
+ *
  * @param[in] src ipv4 address in source byte order.
  * @param[out] dest ipv4 address in target byte order.
  * @param[in] convert_fp Pointer to the function used for conversion.
  * }
-*/
-void lrtr_ipv4_addr_convert_byte_order(const uint32_t src,
-                                       uint32_t *dest,
+ */
+void lrtr_ipv4_addr_convert_byte_order(const uint32_t src, uint32_t *dest,
 				       const enum target_byte_order tbo);
 
 #endif
