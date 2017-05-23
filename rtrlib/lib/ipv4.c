40f6ff547676847d2e73b8d11e9ecacf70e5a196
smlng <s@mlng.net>
2017-05-23 22:40:06 +0200

    lib, ipv4: fix implicit declaration of function lrtr_get_bits

--- rtrlib/lib/ipv4.c
+++ rtrlib/lib/ipv4.c
@@ -9,45 +9,53 @@
 
 #include "rtrlib/lib/convert_byte_order.h"
 #include "rtrlib/lib/ipv4.h"
+#include "rtrlib/lib/utils.h"
 
 #include <assert.h>
 #include <stdio.h>
 
-struct lrtr_ipv4_addr lrtr_ipv4_get_bits(const struct lrtr_ipv4_addr *val, const uint8_t from, const uint8_t quantity)
+struct lrtr_ipv4_addr lrtr_ipv4_get_bits(const struct lrtr_ipv4_addr *val,
+                                         const uint8_t from,
+                                         const uint8_t quantity)
 {
-    struct lrtr_ipv4_addr result;
-    result.addr = lrtr_get_bits(val->addr, from, quantity);
-    return result;
+        struct lrtr_ipv4_addr result;
+        result.addr = lrtr_get_bits(val->addr, from, quantity);
+        return result;
 }
 
-int lrtr_ipv4_addr_to_str(const struct lrtr_ipv4_addr *ip, char *str, unsigned int len)
+int lrtr_ipv4_addr_to_str(const struct lrtr_ipv4_addr *ip,
+                          char *str, unsigned int len)
 {
-    const uint8_t *t = (uint8_t *) &(ip->addr);
+        const uint8_t *t = (uint8_t *) &(ip->addr);
 
-    if (snprintf(str, len, "%hhu.%hhu.%hhu.%hhu", t[3], t[2], t[1], t[0]) < 0) {
-        return -1;
-    }
-    return 0;
+        if (snprintf(str, len, "%hhu.%hhu.%hhu.%hhu",
+                     t[3], t[2], t[1], t[0]) < 0)
+                return -1;
+
+        return 0;
 }
 
 int lrtr_ipv4_str_to_addr(const char *str, struct lrtr_ipv4_addr *ip)
 {
-    unsigned char *t =  (unsigned char *) &(ip->addr);
-    if(sscanf(str, "%3hhu.%3hhu.%3hhu.%3hhu", &(t[3]), &(t[2]), &(t[1]), &(t[0])) != 4)
-        return -1;
-    return 0;
+        unsigned char *t =  (unsigned char *) &(ip->addr);
+        if (sscanf(str, "%3hhu.%3hhu.%3hhu.%3hhu",
+                   &(t[3]), &(t[2]), &(t[1]), &(t[0])) != 4)
+                return -1;
+
+        return 0;
 }
 
-bool lrtr_ipv4_addr_equal(const struct lrtr_ipv4_addr *a, const struct lrtr_ipv4_addr *b)
+bool lrtr_ipv4_addr_equal(const struct lrtr_ipv4_addr *a,
+                          const struct lrtr_ipv4_addr *b)
 {
-    if(a->addr == b->addr)
-        return true;
-    return false;
+        if (a->addr == b->addr)
+                return true;
+
+        return false;
 }
 
-void lrtr_ipv4_addr_convert_byte_order(const uint32_t src,
-                                       uint32_t *dest,
-				       const enum target_byte_order tbo)
+void lrtr_ipv4_addr_convert_byte_order(const uint32_t src, uint32_t *dest,
+                                       const enum target_byte_order tbo)
 {
-	*dest = lrtr_convert_long(tbo, src);
+        *dest = lrtr_convert_long(tbo, src);
 }
