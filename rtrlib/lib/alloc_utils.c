c7e3680041e5a19114b51746c98f09ddca08c9f7
Marcel Röthke <marcel.roethke@haw-hamburg.de>
2017-08-09 17:22:56 +0200

    alloc_utils: fix coding style and add to style check list

--- rtrlib/lib/alloc_utils.c
+++ rtrlib/lib/alloc_utils.c
@@ -11,37 +11,36 @@
 
 #include "alloc_utils.h"
 
-void* (*MALLOC_PTR)(size_t size) = malloc;
-void* (*REALLOC_PTR)(void* ptr, size_t size) = realloc;
-void (*FREE_PTR)(void* ptr) = free;
+void *(*MALLOC_PTR)(size_t size) = malloc;
+void *(*REALLOC_PTR)(void *ptr, size_t size) = realloc;
+void (*FREE_PTR)(void *ptr) = free;
 
-
-void lrtr_set_malloc(void* (*malloc_function)(size_t size))
+void lrtr_set_malloc(void *(*malloc_function)(size_t size))
 {
 	 MALLOC_PTR = malloc_function;
 }
 
-void lrtr_set_realloc(void* (*realloc_ptr)(void* ptr, size_t size))
+void lrtr_set_realloc(void *(*realloc_ptr)(void *ptr, size_t size))
 {
 	REALLOC_PTR = realloc_ptr;
 }
 
-void lrtr_set_free(void (free_ptr)(void* ptr))
+void lrtr_set_free(void (free_ptr)(void *ptr))
 {
 	FREE_PTR = free_ptr;
 }
 
-inline void* lrtr_malloc(size_t size)
+inline void *lrtr_malloc(size_t size)
 {
 	return MALLOC_PTR(size);
 }
 
-inline void lrtr_free(void* ptr)
+inline void lrtr_free(void *ptr)
 {
 	return FREE_PTR(ptr);
 }
 
-inline void* lrtr_realloc(void* ptr, size_t size)
+inline void *lrtr_realloc(void *ptr, size_t size)
 {
 	return REALLOC_PTR(ptr, size);
 }
--- rtrlib/lib/alloc_utils.h
+++ rtrlib/lib/alloc_utils.h
@@ -12,35 +12,35 @@
 
 #include <stdlib.h>
 
-extern void* (*MALLOC_PTR)(size_t size);
-extern void (*FREE_PTR)(void* ptr);
-extern void* (*REALLOC_PTR)(void* ptr, size_t size);
+extern void *(*MALLOC_PTR)(size_t size);
+extern void (*FREE_PTR)(void *ptr);
+extern void *(*REALLOC_PTR)(void *ptr, size_t size);
 
 /**
-* @brief Sets a custom malloc function that is used throughout rtrlib.
-* If you set this function you must also set custom free and custom realloc
-* @param[in] Pointer to a malloc function
-*/
-void lrtr_set_malloc(void* (*malloc_function)(size_t size));
+ * @brief Sets a custom malloc function that is used throughout rtrlib.
+ * If you set this function you must also set custom free and custom realloc
+ * @param[in] Pointer to a malloc function
+ */
+void lrtr_set_malloc(void *(*malloc_function)(size_t size));
 
 /**
-* @brief Sets a custom free function that is used throughout rtrlib.
-* If you set this function you must also set custom malloc and realloc
-* @param[in] Pointer to a free function
-*/
-void lrtr_set_free(void (free_ptr)(void* ptr));
+ * @brief Sets a custom free function that is used throughout rtrlib.
+ * If you set this function you must also set custom malloc and realloc
+ * @param[in] Pointer to a free function
+ */
+void lrtr_set_free(void (free_ptr)(void *ptr));
 
 /**
-* @brief Sets a custom realloc function that is used throughout rtrlib.
-* If you set this function you must also set custom malloc and free.
-* @param[in] Pointer to a realloc function
-*/
-void lrtr_set_realloc(void* (*realloc_ptr)(void* ptr, size_t size));
+ * @brief Sets a custom realloc function that is used throughout rtrlib.
+ * If you set this function you must also set custom malloc and free.
+ * @param[in] Pointer to a realloc function
+ */
+void lrtr_set_realloc(void *(*realloc_ptr)(void *ptr, size_t size));
 
-void* lrtr_malloc(size_t size);
+void *lrtr_malloc(size_t size);
 
-void lrtr_free(void* ptr);
+void lrtr_free(void *ptr);
 
-void* lrtr_realloc(void* ptr, size_t size);
+void *lrtr_realloc(void *ptr, size_t size);
 
 #endif
