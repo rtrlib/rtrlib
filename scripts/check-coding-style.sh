dad197b99768c269917ef30af71befdf6bf20a3f
Marcel Röthke <marcel.roethke@haw-hamburg.de>
2019-03-12 14:38:54 +0100

    scripts: update checkpatch

--- scripts/check-coding-style.sh
+++ scripts/check-coding-style.sh
@@ -29,7 +29,7 @@ fi
 cd $SCRIPT_DIR/..
 for i in $CHECKSOURCE; do
 	echo "> check coding style of $i ..."
-    IGNORE="PREFER_KERNEL_TYPES,CONST_STRUCT,OPEN_BRACE"
+    IGNORE="PREFER_KERNEL_TYPES,CONST_STRUCT,OPEN_BRACE,SPDX_LICENSE_TAG,OPEN_ENDED_LINE,UNNECESSARY_PARENTHESES"
     if [[ $i == *"unittest"* ]]; then
         IGNORE="${IGNORE},CAMELCASE"
     fi
