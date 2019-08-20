c3c10be7663badb62946711f3fe0df15316bba1a
Marcel Röthke <marcel.roethke@haw-hamburg.de>
2019-08-20 15:33:53 +0200

    tests: fix transitive includes

--- tests/unittests/test_packets_static.h
+++ tests/unittests/test_packets_static.h
@@ -7,6 +7,8 @@
  * Website; http://rtrlib.realmv6.org/
  */
 
+#include "rtrlib/transport/transport_private.h"
+
 int __wrap_lrtr_get_monotonic_time(time_t *seconds);
 
 int __wrap_tr_send_all(const struct tr_socket *socket,
