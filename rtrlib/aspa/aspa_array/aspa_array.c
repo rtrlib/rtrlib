f74457afbea1db33bb671ad356f0473cfec06978
revol-xut <revol-xut@protonmail.com>
2024-01-31 09:29:43 +0100

    aspa: add aspa data structures and verification algorithm
    - add `aspa_array`, an ordered dynamic array
    - add `aspa_table` for storing and managing aspa data
    - add aspa table update functions
    - add AS_PATH verification algorithm
    
    Co-authored-by: mrzslz <moritz.schulz@proton.me>
    Co-authored-by: carl <115627588+carl-tud@users.noreply.github.com>

---
+++ rtrlib/aspa/aspa_array/aspa_array.c
@@ -0,0 +1,230 @@
+/*
+ * This file is part of RTRlib.
+ *
+ * This file is subject to the terms and conditions of the MIT license.
+ * See the file LICENSE in the top level directory for more details.
+ *
+ * Website: http://rtrlib.realmv6.org/
+ */
+
+#include "aspa_array.h"
+
+#include "rtrlib/aspa/aspa_private.h"
+#include "rtrlib/lib/alloc_utils_private.h"
+#include "rtrlib/rtr/rtr.h"
+
+// MARK: - Initialization & Deinitialization
+
+enum aspa_status aspa_array_create(struct aspa_array **array_ptr)
+{
+	const size_t default_initial_size = 128;
+
+	// allocation the chunk of memory of the provider as numbers
+	struct aspa_record *data_field = lrtr_malloc(sizeof(struct aspa_record) * default_initial_size);
+
+	// malloc failed so returning an error
+	if (!data_field)
+		return ASPA_ERROR;
+
+	// allocating the aspa_record itself
+	struct aspa_array *array = lrtr_malloc(sizeof(struct aspa_array));
+
+	// malloc for aspa_record failed hence we return an error
+	if (!array) {
+		lrtr_free(data_field);
+		return ASPA_ERROR;
+	}
+
+	// initializing member variables of the aspa record
+	array->capacity = default_initial_size;
+	array->size = 0;
+	array->data = data_field;
+
+	// returning the array
+	*array_ptr = array;
+
+	return ASPA_SUCCESS;
+}
+
+void aspa_array_free(struct aspa_array *array, bool free_provider_arrays)
+{
+	// if the array is null just return
+	if (!array)
+		return;
+
+	if (array->data) {
+		if (free_provider_arrays) {
+			for (size_t i = 0; i < array->size; i++) {
+				if (array->data[i].provider_asns) {
+					lrtr_free(array->data[i].provider_asns);
+					array->data[i].provider_asns = NULL;
+				}
+			}
+		}
+
+		// freeing the data
+		lrtr_free(array->data);
+	}
+
+	// freeing the array itself
+	lrtr_free(array);
+}
+
+// MARK: - Manipulation
+
+static enum aspa_status aspa_array_reallocate(struct aspa_array *array)
+{
+	// the factor by how much the capacity will increase: new_capacity = old_capacity * SIZE_INCREASE_EXPONENTIAL
+	const size_t SIZE_INCREASE_EXPONENTIAL = 2;
+
+	// allocation the new chunk of memory
+	struct aspa_record *tmp =
+		lrtr_realloc(array->data, sizeof(struct aspa_record) * array->capacity * SIZE_INCREASE_EXPONENTIAL);
+
+	// malloc failed so returning an error
+	if (!tmp)
+		return ASPA_ERROR;
+
+	array->data = tmp;
+	array->capacity *= SIZE_INCREASE_EXPONENTIAL;
+	return ASPA_SUCCESS;
+}
+
+static enum aspa_status aspa_array_insert_internal(struct aspa_array *array, size_t index, struct aspa_record *record,
+						   bool copy_providers)
+{
+	// check if this element will fit into the array
+	if (array->size >= array->capacity) {
+		// increasing the array's size so the new element fits
+		if (aspa_array_reallocate(array) != ASPA_SUCCESS)
+			return ASPA_ERROR;
+	}
+
+	uint32_t *provider_asns = NULL;
+
+	if (record->provider_count > 0) {
+		if (copy_providers) {
+			size_t provider_size = record->provider_count * sizeof(uint32_t);
+
+			provider_asns = lrtr_malloc(provider_size);
+			if (!provider_asns)
+				return ASPA_ERROR;
+
+			memcpy(provider_asns, record->provider_asns, provider_size);
+		} else {
+			provider_asns = record->provider_asns;
+		}
+	}
+
+	// No need to move if last element
+	if (index < array->size) {
+		size_t trailing = (array->size - index) * sizeof(struct aspa_record);
+
+		/*               trailing
+		 *		     /-------------\
+		 * #3 #8 #11 #24 #30 #36 #37
+		 * #3 #8 #11  *  #24 #30 #36 #37
+		 *            ^   ^
+		 *		index   index + 1
+		 */
+		memmove(&array->data[index + 1], &array->data[index], trailing);
+	}
+
+	// append the record at the end
+	array->data[index] = *record;
+	array->data[index].provider_asns = provider_asns;
+	array->size += 1;
+
+	return ASPA_SUCCESS;
+}
+
+enum aspa_status aspa_array_insert(struct aspa_array *array, size_t index, struct aspa_record *record,
+				   bool copy_providers)
+{
+	if (!array || index > array->size || !array->data || !record ||
+	    ((record->provider_count > 0) && !record->provider_asns))
+		return ASPA_ERROR;
+
+	return aspa_array_insert_internal(array, index, record, copy_providers);
+}
+
+enum aspa_status aspa_array_append(struct aspa_array *array, struct aspa_record *record, bool copy_providers)
+{
+	if (!array || !array->data || !record || ((record->provider_count > 0) && !record->provider_asns))
+		return ASPA_ERROR;
+
+	return aspa_array_insert_internal(array, array->size, record, copy_providers);
+}
+
+enum aspa_status aspa_array_remove(struct aspa_array *array, size_t index, bool free_providers)
+{
+	if (!array || index >= array->size || array->size == 0)
+		return ASPA_RECORD_NOT_FOUND;
+
+	if (free_providers && array->data[index].provider_asns)
+		lrtr_free(array->data[index].provider_asns);
+
+	// No need to move if last element
+	if (index < array->size - 1) {
+		size_t trailing = (array->size - index - 1) * sizeof(struct aspa_record);
+
+		/*                     trailing
+		 *				   /-------------\
+		 *   #3 #8 #11  *  #24 #30 #36 #37
+		 *   #3 #8 #11 #24 #30 #36 #37
+		 *			    ^   ^
+		 *		    index   index + 1
+		 */
+		memmove(&array->data[index], &array->data[index + 1], trailing);
+	}
+
+	array->size -= 1;
+	return ASPA_SUCCESS;
+}
+
+inline struct aspa_record *aspa_array_get_record(struct aspa_array *array, size_t index)
+{
+	if (!array || index >= array->size || array->size == 0 || !array->data)
+		return NULL;
+
+	return &array->data[index];
+}
+
+// MARK: - Retrieval
+
+struct aspa_record *aspa_array_search(struct aspa_array *array, uint32_t customer_asn)
+{
+	// if the array is empty we return an error
+	if (array->size == 0 || array->capacity == 0)
+		return NULL;
+
+	// left and right bound of our search space
+	register size_t left = 0;
+	register size_t right = array->size - 1;
+
+	// we stop if right and left crossed
+	while (left <= right) {
+		// current center
+		size_t center = (left + right) >> 1;
+		uint32_t center_value = array->data[center].customer_asn;
+
+		// success found the value
+		if (center_value == customer_asn) {
+			return &array->data[center];
+
+			// value should be on the right side
+		} else if (center_value < customer_asn) {
+			left = center + 1;
+
+			// value should be on the left side
+		} else if (center == 0) {
+			// value cannot be left of index 0
+			return NULL;
+		} else {
+			right = center - 1;
+		}
+	}
+
+	// element not found
+	return NULL;
+}
---
+++ rtrlib/aspa/aspa_array/aspa_array.h
@@ -0,0 +1,106 @@
+/*
+ * This file is part of RTRlib.
+ *
+ * This file is subject to the terms and conditions of the MIT license.
+ * See the file LICENSE in the top level directory for more details.
+ *
+ * Website: http://rtrlib.realmv6.org/
+ */
+
+#ifndef RTR_ASPA_DYN_ARRAY_H
+#define RTR_ASPA_DYN_ARRAY_H
+
+#include "../aspa.h"
+
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * @brief Struct which is similar in function to std::vector from C++.
+ * If the vector is running full, a larger chunk of memory is reallocated.
+ *
+ * This structure stores ASPA records in a contiguous chunk of memory,
+ * sorted in ascending order by their customer ASN.
+ */
+struct aspa_array {
+	uint32_t size;
+	size_t capacity;
+	struct aspa_record *data;
+};
+
+// MARK: - Initialization & Deinitialization
+
+/**
+ * @brief Creates an vector object
+ * @param[in,out] array_ptr Pointer to a variable that will hold a reference to the newly created array.
+ * @return @c ASPA_SUCCESS if the operation succeeds, @c ASPA_ERROR if it fails.
+ */
+enum aspa_status aspa_array_create(struct aspa_array **array_ptr);
+
+/**
+ * @brief Deletes the given ASPA array.
+ * @param array ASPA array which will be deleted
+ * @param free_provider_arrays A boolean value determining whether each record's provider array should be deallocated.
+ */
+void aspa_array_free(struct aspa_array *array, bool free_provider_arrays);
+
+// MARK: - Manipulation
+
+/**
+ * @brief Inserts a given ASPA record into the array, preserving its order.
+ *
+ * @param array The ASPA array that will hold the new record.
+ * @param index The index at which the new record will be stored.
+ * @param record The new record.
+ * @param copy_providers A boolean value indicating whether the array should copy the record's
+ * providers before inserting the record.
+ * @return @c ASPA_SUCCESS if the operation succeeds, @c ASPA_ERROR if it fails.
+ */
+enum aspa_status aspa_array_insert(struct aspa_array *array, size_t index, struct aspa_record *record,
+				   bool copy_providers);
+
+/**
+ * @brief Appends a given ASPA record to the array.
+ *
+ * @param array The ASPA array that will hold the new record.
+ * @param record The record that will be appended to the array.
+ * @param copy_providers A boolean value indicating whether the array should copy the record's
+ * providers before appending the record.
+ * @return @c ASPA_SUCCESS if the operation succeeds, @c ASPA_ERROR if it fails.
+ */
+enum aspa_status aspa_array_append(struct aspa_array *array, struct aspa_record *record, bool copy_providers);
+
+/**
+ * @brief Removes the record at the given index from the array.
+ *
+ * @param array The array to remove the record from.
+ * @param index The record's index.
+ * @param free_providers A boolean value determining whether to free the existing record's provider array.
+ * @return @c ASPA_SUCCESS if the operation succeeds, @c ASPA_RECORD_NOT_FOUND if the record's index doesn't exist,
+ * @c ASPA_ERROR otherwise.
+ */
+enum aspa_status aspa_array_remove(struct aspa_array *array, size_t index, bool free_providers);
+
+// MARK: - Retrieval
+
+/**
+ * @brief Returns a reference to the record at the given index.
+ *
+ * @param array ASPA array
+ * @param index The index in the ASPA array.
+ * @return A reference to the `aspa_record` if found, @c NULL otherwise.
+ */
+struct aspa_record *aspa_array_get_record(struct aspa_array *array, size_t index);
+
+/**
+ * @brief Searches the given ASPA array for a record matching its customer ASN.
+ *
+ * @param array The array to search.
+ * @param customer_asn Customer ASN
+ * @return A reference to the `aspa_record` if found, @c NULL otherwise.
+ */
+struct aspa_record *aspa_array_search(struct aspa_array *array, uint32_t customer_asn);
+
+#endif
